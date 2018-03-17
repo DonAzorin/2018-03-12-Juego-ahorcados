f79a27c203654f58166efe26d6e0fedce1b2d92b
Moises Alvarado Velazquez <moalve7690@outlook.com>
2018-03-16 22:06:07 -0600

    added basic design to index html y functions to make hangman game

---
+++ script.js
@@ -0,0 +1,31 @@
+function listaPalabras() {
+  var reactivo = Math.floor(Math.random()*10)
+  console.log(reactivo);
+
+    if (reactivo == 0) {
+      reactivo = '[j,u,a,n,e,p,o,m,u,c,e,n,o,a,l,m,o,n,t,e]'
+    } else if (reactivo == 1) {
+      reactivo = '[m,c,l,a,n,e,o,c,a,m,p,o]'
+    } else if (reactivo == 2) {
+      reactivo = '[m,o,n,a,l,m,o,n,t,e]'
+    } else if (reactivo == 3) {
+      reactivo = '[p,l,a,n,d,e,t,u,x,t,e,p,e,c]'
+    } else if (reactivo == 4) {
+      reactivo = '[l,e,y,e,s,d,e,p,r,e,r,e,f,o,r,m,a]'
+    } else if (reactivo == 5) {
+      reactivo = '[t,r,a,t,a,d,o,s,p,r,e,l,i,m,i,n,a,r,e,s,d,e,l,a,s,o,l,e,d,a,d]'
+    } else if (reactivo == 6) {
+      reactivo = '[f,e,l,i,x,m,a,r,i,a,z,u,l,o,a,g,a]'
+    } else if (reactivo == 7) {
+      reactivo = '[d,o,c,t,r,i,n,a,m,o,n,r,o,e]'
+    } else if (reactivo == 8) {
+      reactivo = '[j,u,a,n,a,l,v,a,r,e,z]'
+    } else if (reactivo == 9) {
+      reactivo = '[p,l,a,n,d,e,t,a,c,u,b,a,y,a]'
+    }
+  console.log(reactivo);
+}
+
+// function verificaletras () {
+//
+// }
