e4d7cd486bda407ffdb466848e40e3a59ecd2f70
Moises Alvarado Velazquez <moalve7690@outlook.com>
2018-03-22 22:01:22 -0600

    added a table that changes its cells in function to te lenght of the word to search and simplified index html

--- script.js
+++ script.js
@@ -1,31 +1,62 @@
-function listaPalabras() {
-  var reactivo = Math.floor(Math.random()*10)
-  console.log(reactivo);
+'use strict';
+var reactive;
 
-    if (reactivo == 0) {
-      reactivo = '[j,u,a,n,e,p,o,m,u,c,e,n,o,a,l,m,o,n,t,e]'
-    } else if (reactivo == 1) {
-      reactivo = '[m,c,l,a,n,e,o,c,a,m,p,o]'
-    } else if (reactivo == 2) {
-      reactivo = '[m,o,n,a,l,m,o,n,t,e]'
-    } else if (reactivo == 3) {
-      reactivo = '[p,l,a,n,d,e,t,u,x,t,e,p,e,c]'
-    } else if (reactivo == 4) {
-      reactivo = '[l,e,y,e,s,d,e,p,r,e,r,e,f,o,r,m,a]'
-    } else if (reactivo == 5) {
-      reactivo = '[t,r,a,t,a,d,o,s,p,r,e,l,i,m,i,n,a,r,e,s,d,e,l,a,s,o,l,e,d,a,d]'
-    } else if (reactivo == 6) {
-      reactivo = '[f,e,l,i,x,m,a,r,i,a,z,u,l,o,a,g,a]'
-    } else if (reactivo == 7) {
-      reactivo = '[d,o,c,t,r,i,n,a,m,o,n,r,o,e]'
-    } else if (reactivo == 8) {
-      reactivo = '[j,u,a,n,a,l,v,a,r,e,z]'
-    } else if (reactivo == 9) {
-      reactivo = '[p,l,a,n,d,e,t,a,c,u,b,a,y,a]'
+function listWords() {
+  reactive = Math.floor(Math.random()*10)
+
+    if (reactive == 0) {
+      reactive = 'juannepomucenoalmonte'
+    } else if (reactive == 1) {
+      reactive = 'mclaneocampo'
+    } else if (reactive == 2) {
+      reactive = 'monalmonte'
+    } else if (reactive == 3) {
+      reactive = 'plandetuxtepec'
+    } else if (reactive == 4) {
+      reactive = 'leyesdereforma'
+    } else if (reactive == 5) {
+      reactive = 'tratadospreliminaresdelasoledad'
+    } else if (reactive == 6) {
+      reactive = 'felixmariazuloaga'
+    } else if (reactive == 7) {
+      reactive = 'doctrinamonroe'
+    } else if (reactive == 8) {
+      reactive = 'juanalvarez'
+    } else if (reactive == 9) {
+      reactive = 'plandetacubaya'
     }
-  console.log(reactivo);
+  console.log(reactive);
+  makeTable (reactive);
 }
 
-// function verificaletras () {
-//
-// }
+function clickLetter (letter) {
+  var selectedLetter = letter.value;
+  console.log(selectedLetter);
+  compareLetter (selectedLetter);
+}
+
+document.addEventListener('keydown', function getKeyLetter(event) {
+  var selectedLetter = event.key.toLowerCase();
+  console.log(selectedLetter);
+  compareLetter (selectedLetter);
+})
+
+function compareLetter (selectedLetter) {
+  console.log(reactive.length);
+  for (var i = 0; i < reactive.length; i++) {
+    if (selectedLetter == reactive [i]){
+      console.log('chingon');
+    } else {
+      console.log('fail');
+    }
+  }
+}
+
+function makeTable () {
+  document.getElementById('reactive').deleteRow(0);
+  var table = document.getElementById('reactive');
+  var row =  table.insertRow (0);
+  for (var i = 0; i < reactive.length; i++) {
+    var cell = row.insertCell (i)
+  };
+}
