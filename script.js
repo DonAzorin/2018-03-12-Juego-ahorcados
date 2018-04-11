0e28b7126f114076b7362869369237e125e2b5b6
Moises Alvarado Velazquez <moalve7690@outlook.com>
2018-04-10 19:15:06 -0500

    Optimization of functions and modified index.html to link new function in script.js

--- script.js
+++ script.js
@@ -8,35 +8,19 @@ var score = {
 var checkWin = 0;
 var checkLose = 0;
 
-function listWords() {
-  reactive = Math.floor(Math.random()*10)
+var listWords = ['juannepomucenoalmonte', 'mclaneocampo', 'monalmonte', 'monalmonte', 'plandetuxtepec', 'leyesdereforma', 'tratadospreliminaresdelasoledad', 'felixmariazuloaga', 'doctrinamonroe', 'juanalvarez', 'plandetacubaya']
 
-    if (reactive == 0) {
-      reactive = 'juannepomucenoalmonte'
-    } else if (reactive == 1) {
-      reactive = 'mclaneocampo'
-    } else if (reactive == 2) {
-      reactive = 'monalmonte'
-    } else if (reactive == 3) {
-      reactive = 'plandetuxtepec'
-    } else if (reactive == 4) {
-      reactive = 'leyesdereforma'
-    } else if (reactive == 5) {
-      reactive = 'tratadospreliminaresdelasoledad'
-    } else if (reactive == 6) {
-      reactive = 'felixmariazuloaga'
-    } else if (reactive == 7) {
-      reactive = 'doctrinamonroe'
-    } else if (reactive == 8) {
-      reactive = 'juanalvarez'
-    } else if (reactive == 9) {
-      reactive = 'plandetacubaya'
-    }
+function selectWord() {
+  reactive = listWords [Math.floor (Math.random() * listWords.length )];
   console.log(reactive);
+  restoreGame (reactive)
+  makeTable (reactive);
+  }
 
   //Usa una API... Quizá la de R&M :/
   // fetch('')
 
+function restoreGame() {
   document.getElementById('failText').innerHTML = '0';
   document.getElementById('errores').src = 'images/0error.jpg';
   checkWin = 0;
@@ -46,20 +30,22 @@ function listWords() {
     var buttonList = showButton[i].id
     document.getElementById(buttonList).style.visibility = 'visible';
   }
+}
 
-  makeTable (reactive);
+function hideLetter(selectedLetter) {
+  document.getElementById(selectedLetter).style.visibility = 'hidden';
 }
 
 function clickLetter (letter) {
   var selectedLetter = letter.value;
   compareLetter (selectedLetter);
-  document.getElementById(selectedLetter).style.visibility = 'hidden';
+  hideLetter (selectedLetter);
 }
 
 document.addEventListener('keydown', function getKeyLetter(event) {
   var selectedLetter = event.key.toLowerCase();
   compareLetter (selectedLetter);
-  document.getElementById(selectedLetter).style.visibility = 'hidden';
+  hideLetter (selectedLetter);
 })
 
 function compareLetter (selectedLetter) {
@@ -72,33 +58,29 @@ function compareLetter (selectedLetter) {
       flag ++;
     }
   }
-
   if (checkWin == reactive.length) {
     score.win = score.win + 1;
     winText.innerHTML = score.win;
   } else if (flag == reactive.length) {
     score.fail = score.fail + 1;
     failText.innerHTML = score.fail;
+    failImage (score.fail);
   }
   //coloca una capa superior que no permita clickLetter o selectedLetter y tenga el botón de listWords
+}
 
+function failImage() {
   for(let i = 1; i <= 6; i++) {
     if(score.fail === i) {
       document.getElementById('errores').src = 'images/' + i + 'error.jpg';
     }
   }
-
   if (score.fail === 7) {
     document.getElementById('errores').src = 'images/7error.jpg';
     score.lose = score.lose + 1;
     loseText.innerHTML = score.lose;
-
-
-    // si 7 es igual a 1, envía un innerHTML que se imprima en palabras erróneas
-    //coloca una capa superior que no permita clickLetter o selectedLetter y tenga el botón de listWords
-
-
   }
+    //coloca una capa superior que no permita clickLetter o selectedLetter y tenga el botón de listWords
 }
 
 function makeTable () {
